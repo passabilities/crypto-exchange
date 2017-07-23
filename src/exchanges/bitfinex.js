8bf51338190d905ce83d8e5b6b79fe04c0a3c8fe
Noah Passalacqua <noah.passa@gmail.com>
2017-07-23 02:02:10 -0700

    Add Bitfinex.

---
+++ src/exchanges/bitfinex.js
@@ -0,0 +1,147 @@
+const Api = require('bitfinex')
+const _ = require('lodash')
+
+const Pair = require('../../lib/pair')
+
+const { key, secret } = require('../getKeys')('bitfinex')
+const bitfinex = new Api(key, secret)
+
+/**
+ * NOTE: Bittrex API returns pairs in "quote-base" order.
+ */
+
+class Bitfinex {
+
+  // Public Methods
+
+  ticker(pair) {
+    return new Promise((resolve, reject) => {
+      pair = pair.replace('_','')
+      bitfinex.ticker(pair,
+        (err, tick) => {
+          if(err) {
+            reject(err.message)
+          } else {
+            let { last_price, ask, bid, high, low, volume, timestamp } = tick
+            resolve({
+              last: parseFloat(last_price),
+              ask: parseFloat(ask),
+              bid: parseFloat(bid),
+              high: parseFloat(high),
+              low: parseFloat(low),
+              volume: parseFloat(volume),
+              timestamp: Math.floor(parseFloat(timestamp) * 1000)
+            })
+          }
+        })
+    })
+  }
+
+  assets() {
+    return new Promise((resolve, reject) => {
+      this.pairs()
+        .then( pairs => {
+          let assets = _.reduce(pairs, (result, p) => result.concat(p.split('_')), [])
+          assets = _.uniq(assets)
+          resolve(assets)
+        })
+        .catch(reject)
+    })
+  }
+
+  pairs() {
+    return new Promise((resolve, reject) => {
+      bitfinex.get_symbols(
+        (err, pairs) => {
+          if(err) {
+            reject(err.message)
+          } else {
+            resolve(_.map(pairs, p => (
+              p.replace(/(.{3})(.{3})/, '$1_$2')
+                .toUpperCase()
+            )))
+          }
+        })
+    })
+  }
+
+  depth(pair, count=50) {
+    return new Promise((resolve, reject) => {
+      pair = pair.replace('_','')
+      bitfinex.orderbook(pair, { limit_asks: count, limit_bids: count },
+        (err, depth) => {
+          if(err) {
+            reject(err.message)
+          } else {
+            depth = { buy: depth.bids, sell: depth.asks }
+            _.each(depth, (entries, type) => {
+              depth[type] = _.map(entries, entry => {
+                return [
+                  parseFloat(entry.price),
+                  parseFloat(entry.amount)
+                ]
+              })
+            })
+            resolve(depth)
+          }
+        })
+    })
+  }
+
+  // Authenticated Methods
+
+  buy() {
+    return privateMethods.addOrder.apply(this, ['buy', ...arguments])
+  }
+
+  sell() {
+    return privateMethods.addOrder.apply(this, ['sell', ...arguments])
+  }
+
+  balances() {
+    return new Promise((resolve, reject) => {
+      bitfinex.wallet_balances(
+        (err, balances) => {
+          if(err) {
+            reject(err.message)
+          } else {
+            balances = _.filter(balances, b => b.type === 'exchange')
+            balances = _.map(balances, (result, b) => {
+              let balance = parseFloat(b.amount)
+              let available = parseFloat(b.available)
+              return {
+                asset: b.currency.toUpperCase(),
+                balance,
+                available,
+                pending: balance - available
+              }
+            })
+            resolve(balances)
+          }
+        })
+    })
+  }
+
+}
+
+module.exports = Bitfinex
+
+const privateMethods = {
+
+  addOrder(type, pair, amount, rate) {
+    return new Promise((resolve, reject) => {
+      pair = pair.replace('_','')
+      amount = amount.toString()
+      rate = rate.toString()
+      bitfinex.new_order(pair, amount, rate, 'bitfinex', type, 'limit',
+        (err, res) => {
+          if(err) {
+            reject(err.message)
+          } else {
+            resolve(res)
+          }
+        })
+    })
+  }
+
+}
