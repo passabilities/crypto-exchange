19f470d2d4bcf544f873f363ebe03521e7c4d20c
Noah Passalacqua <noah.passa@gmail.com>
2018-04-03 23:51:51 -0400

    Double space bitstamp page.
    (cherry picked from commit 330a070555b057173bde71255ad061e0dc753c53)

--- src/exchanges/bitstamp.js
+++ src/exchanges/bitstamp.js
@@ -1,121 +1,118 @@
 const Api = require('bitstamp')
 const _ = require('lodash')
 
-
-
 /**
  * NOTE: Bittrex API returns pairs in "quote-base" order.
  */
 
 class Bitstamp {
 
+  constructor({ key, secret }) {
+    this.bitstamp = new Api(key, secret)
+  }
 
-    constructor({ key, secret }) {
-        this.bitstamp = new Api(key, secret)
-    }
 
-
-    ticker(pair) {
-        return new Promise((resolve, reject) => {
-            pair = pair.replace('_', '').toLowerCase()
-            this.bitstamp.ticker(pair,
-                (err, tick) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        let {last, ask, bid, high, low, volume, timestamp} = tick
-                        resolve({
-                            last: parseFloat(last),
-                            ask: parseFloat(ask),
-                            bid: parseFloat(bid),
-                            high: parseFloat(high),
-                            low: parseFloat(low),
-                            volume: parseFloat(volume),
-                            timestamp: Math.floor(parseFloat(timestamp) * 1000)
-                        })
-                    }
-                })
+  ticker(pair) {
+    return new Promise((resolve, reject) => {
+      pair = pair.replace('_', '').toLowerCase()
+      this.bitstamp.ticker(pair,
+        (err, tick) => {
+          if (err) {
+            reject(err)
+          } else {
+            let {last, ask, bid, high, low, volume, timestamp} = tick
+            resolve({
+              last: parseFloat(last),
+              ask: parseFloat(ask),
+              bid: parseFloat(bid),
+              high: parseFloat(high),
+              low: parseFloat(low),
+              volume: parseFloat(volume),
+              timestamp: Math.floor(parseFloat(timestamp) * 1000)
+            })
+          }
         })
-    }
+    })
+  }
 
-    assets() {
-        return new Promise((resolve, reject) => {
-            this.pairs()
-                .then(pairs => {
-                    let assets = _.reduce(pairs, (result, p) => result.concat(p.split('_')), [])
-                    assets = _.uniq(assets)
-                    resolve(assets)
-                })
+  assets() {
+    return new Promise((resolve, reject) => {
+      this.pairs()
+        .then(pairs => {
+          let assets = _.reduce(pairs, (result, p) => result.concat(p.split('_')), [])
+          assets = _.uniq(assets)
+          resolve(assets)
         })
-    }
+    })
+  }
 
-    pairs() {
-        return new Promise((resolve, reject) => {
-            resolve([
-                'BTC_USD',
-                'BTC_EUR',
-                'EUR_USD',
-                'XRP_USD',
-                'XRP_EUR',
-                'XRP_BTC',
-                'LTC_USD',
-                'LTC_EUR',
-                'LTC_BTC'
-            ])
+  pairs() {
+    return new Promise((resolve, reject) => {
+      resolve([
+        'BTC_USD',
+        'BTC_EUR',
+        'EUR_USD',
+        'XRP_USD',
+        'XRP_EUR',
+        'XRP_BTC',
+        'LTC_USD',
+        'LTC_EUR',
+        'LTC_BTC'
+      ])
+    })
+  }
+
+  depth(pair, count = 50) {
+    return new Promise((resolve, reject) => {
+      pair = pair.replace('_', '')
+      this.bitstamp.order_book(pair,
+        (err, depth) => {
+          if (err) {
+            reject(err.message)
+          } else {
+            depth = _.pick(depth, ['asks', 'bids'])
+            _.each(depth, (entries, type) => {
+              entries = entries.slice(0, count)
+              depth[type] = _.map(entries, e => _.map(e, parseFloat))
+            })
+            resolve(depth)
+          }
         })
-    }
+    })
+  }
 
-    depth(pair, count = 50) {
-        return new Promise((resolve, reject) => {
-            pair = pair.replace('_', '')
-            this.bitstamp.order_book(pair,
-                (err, depth) => {
-                    if (err) {
-                        reject(err.message)
-                    } else {
-                        depth = _.pick(depth, ['asks', 'bids'])
-                        _.each(depth, (entries, type) => {
-                            entries = entries.slice(0, count)
-                            depth[type] = _.map(entries, e => _.map(e, parseFloat))
-                        })
-                        resolve(depth)
-                    }
-                })
+  // Authenticated Methods
+
+  buy() {
+    return privateMethods.addOrder.apply(this, ['buy', ...arguments])
+  }
+
+  sell() {
+    return privateMethods.addOrder.apply(this, ['sell', ...arguments])
+  }
+
+  balances() {
+    return new Promise((resolve, reject) => {
+      this.bitstamp.balance(
+        (err, balances) => {
+          if (err) {
+            reject(err.message)
+          } else {
+            balances = _.reduce(balances, (result, b, key) => {
+              let [asset, access] = key.split('_')
+              asset = asset.toUpperCase()
+              access = access === 'reserved' ? 'pending' : access
+
+              let balance = result[asset] || {asset}
+              balance[access] = parseFloat(b)
+
+              return result
+            }, {})
+            resolve(balances)
+          }
         })
-    }
-
-    // Authenticated Methods
-
-    buy() {
-        return privateMethods.addOrder.apply(this, ['buy', ...arguments])
-    }
-
-    sell() {
-        return privateMethods.addOrder.apply(this, ['sell', ...arguments])
-    }
-
-    balances() {
-        return new Promise((resolve, reject) => {
-            this.bitstamp.balance(
-                (err, balances) => {
-                    if (err) {
-                        reject(err.message)
-                    } else {
-                        balances = _.reduce(balances, (result, b, key) => {
-                            let [asset, access] = key.split('_')
-                            asset = asset.toUpperCase()
-                            access = access === 'reserved' ? 'pending' : access
-
-                            let balance = result[asset] || {asset}
-                            balance[access] = parseFloat(b)
-
-                            return result
-                        }, {})
-                        resolve(balances)
-                    }
-                })
-        })
-    }
+    })
+  }
 
 }
 
@@ -123,20 +120,20 @@ module.exports = Bitstamp
 
 const privateMethods = {
 
-    addOrder(type, pair, amount, rate) {
-        return new Promise((resolve, reject) => {
-            pair = pair.replace('_', '')
-            amount = amount.toString()
-            rate = rate.toString()
-            this.bitstamp[type](pair, amount, rate, null,
-                (err, res) => {
-                    if (err) {
-                        reject(err.message)
-                    } else {
-                        resolve(res)
-                    }
-                })
+  addOrder(type, pair, amount, rate) {
+    return new Promise((resolve, reject) => {
+      pair = pair.replace('_', '')
+      amount = amount.toString()
+      rate = rate.toString()
+      this.bitstamp[type](pair, amount, rate, null,
+        (err, res) => {
+          if (err) {
+            reject(err.message)
+          } else {
+            resolve(res)
+          }
         })
-    }
+    })
+  }
 
 }
